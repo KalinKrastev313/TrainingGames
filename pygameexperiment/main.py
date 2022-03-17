1cd88eed30ff2a99623c289e38a2ac74ce5b922d
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2022-03-17 17:06:41 +0200

    Spaceships rotated

--- pygameexperiment/main.py
+++ pygameexperiment/main.py
@@ -9,13 +9,15 @@ FPS = 60
 
 yellow_spaceship_image = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
 yellow_spaceship = pygame.transform.scale(yellow_spaceship_image, (55, 44))
+yellow_spaceship = pygame.transform.rotate(yellow_spaceship, 90)
 red_spaceship_image = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
 red_spaceship = pygame.transform.scale(red_spaceship_image, (55, 44))
+red_spaceship = pygame.transform.rotate(red_spaceship, 180)
 
 
 def draw_window():
     win.fill((255, 255, 255))
-    win.blit(yellow_spaceship_image, (300, 100))
+    win.blit(yellow_spaceship, (300, 100))
     pygame.display.update()
 
 
